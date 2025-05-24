f338057dab128a625d7f4d2ea1cd83c123959181
ADITYAKUMARRAI2007 <adityarai04012007@gmail.com>
2025-05-24 13:56:23 +0530

    Update CallRecorder.ts

--- src/services/CallRecorder.ts
+++ src/services/CallRecorder.ts
@@ -1,10 +1,14 @@
-import {NativeModules,PermissionAndroid,Platform,ToastAndroid} from 'react-native';
+import {NativeModules,PermissionAndroid,Platform,ToastAndroid} from 'react-native'
 const{CallRecording}=NativeModules
 export const startRecording=async()=>{
     if(Platform.Os!=='Android')return
     const granted=await PermissionAndroid.requestMultiple([
         PermissionAndroid.PERMISSIONS.RECORD_AUDIO,
         PermissionAndroid.PERMISSIONS.READ_PHONE_STATE,
-        
+        PermissionAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        PermissionAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        PermissionAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        PermissionAndroid.PERMISSIONS.PROCESS_OUTGOING_CALLS
     ])
 }
